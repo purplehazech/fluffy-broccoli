0eb8ab37203c7f2807c25ead10f7acc0623e5a90
Lucas Bickel <hairmare@purplehaze.ch>
2015-09-18 13:58:58 +0200

    initial angular project generated by yo

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name fluffyBroccoliApp
+ * @description
+ * # fluffyBroccoliApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('fluffyBroccoliApp', [
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fluffyBroccoliApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fluffyBroccoliApp
+ */
+angular.module('fluffyBroccoliApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
